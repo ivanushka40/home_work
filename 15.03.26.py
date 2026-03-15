efaf1165bc2be8a7cd9bfb93296e2982f1bc274b
ivanushka40 <ivanuskinivan93@gmail.com>
2026-03-15 13:47:54 +0300

    Add files via upload
    15.03.2026

---
+++ 15.03.26.py
@@ -0,0 +1,44 @@
+# for i in range (5):
+#     for j in range (8):
+#         print (3, end = "  ")
+#     print()
+
+# for i in range (10):
+#     for j in range (6):
+#         print (i, end = "  ")
+#     print()
+
+# for i in range (5):
+#     for j in range (10):
+#         print (j, end = "  ")
+#     print ()
+
+# for i in range (1-1, 90, 10):
+#     for j in range (5):
+#         print (i, end = "  ")
+#     print ()
+
+# for n in range (1, 9):
+#     for m in range(4):
+#         print (10 * n + 4, end = ' ')
+#     print()
+
+# for n in range (4):
+#     for m in range(2, 21):
+#         print (m, end = ' ')
+#     print()
+
+# for i in range (5):
+#     for j in range(10, 0, -1):
+#         print (j, end = ' ')
+#     print()
+
+# for n in range (7):
+#     for m in range(n, 10):
+#         print (5, end = ' ')
+#     print()
+
+# for n in range (8):
+#     for m in range(8, n, -1):
+#         print (m, end = ' ')
+#     print()
