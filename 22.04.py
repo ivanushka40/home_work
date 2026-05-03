f73a914b4f4b12595482538cd952c50580d5a8a1
ivanushka40 <ivanuskinivan93@gmail.com>
2026-05-03 14:46:09 +0300

    Add files via upload
    massive

---
+++ 22.04.py
@@ -0,0 +1,85 @@
+# list = ["Москва", "Иваново", "Ростов", "Таганрог", "Азов"]
+# print(list)
+import random
+
+# list = ["Москва", "Иваново", "Ростов", "Таганрог", "Азов"]
+# print(list)
+# print(list[0], list[2])
+
+# list = ["Москва", "Иваново", "Ростов", "Таганрог", "Азов"]
+# print(list)
+# print(list[0], list[2])
+# list[0], list[2] = list[2], list[0]
+# print(list)
+
+# students =[4, 5, 4, 5, 3, 3 , 5, 4, 4, 2]
+# print(students)
+# print(students[4], students[9])
+
+# students =[4, 5, 4, 5, 3, 3 , 5, 4, 4, 2]
+# print(students)
+# print(students[4], students[9])
+# students.append(4)
+# students.append(5)
+# print(students)
+
+# names = str(input())
+# print([names])
+
+# numbers = str(input())
+# print([numbers])
+# print(numbers)
+
+
+# num = int(input())
+# a = [0] * num
+# for i in range(len(a)):
+#     a[i] = int(input())
+# for el in a:
+#     print(el, end = ' ')
+
+# import random
+# a = int(input())
+# list = [random.randint(10, 99) for i in range(a)]
+# print(list)
+
+# a = int(input())
+# list = '*'
+# for i in range(a):
+#     print(list, end =" ")
+
+
+# a = int(input())
+# list = [2 * i - 1 for i in range(a)]
+# print(list)
+
+# import random
+# a = int(input())
+# list1 = [random.randint(1,100) for i in range(a)]
+# print(list1)
+# list2 = []
+# for i in range(len(list1)):
+#     if i%2 ==0:
+#         list2.append(i)
+# print(list2)
+
+
+# n = int(input())
+# b1 = float(input())
+# q = float(input())
+# b = [0] * n
+# b[0] = b1
+# for i in range(1, len(b)):
+#     b[i] = b[i - 1] * q
+# print()
+# for i in range(len(b)):
+#     print (b[i], end = ' ')
+
+# a = int(input())
+# list = [random.randint(1,100) for i in range(a)]
+# print(list)
+# random.shuffle(list)
+# print(list)
+# for i in range(a):
+#     if i(a) = ma
+
